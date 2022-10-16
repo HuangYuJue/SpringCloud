4bb4a5ffdd6939a74042e884779c8537cbfe7145
HuangYuJue <11392488+huang-yujue@user.noreply.gitee.com>
2022-10-16 23:02:09 +0800

    Ribbon负载规则替换

---
+++ consumer-order80/src/main/java/com/example/myrule/MySelfRule.java
@@ -0,0 +1,16 @@
+package com.example.myrule;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import com.netflix.loadbalancer.RoundRobinRule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MySelfRule {
+    @Bean
+    public IRule myRule(){
+        //定义为随机，因为默认为轮询,即RoundRobinRule
+        return new RandomRule();
+    }
+}
