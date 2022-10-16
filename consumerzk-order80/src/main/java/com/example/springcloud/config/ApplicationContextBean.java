3fe362109bc5524717956162ae5c08911f6e0455
HuangYuJue <11392488+huang-yujue@user.noreply.gitee.com>
2022-10-16 14:12:21 +0800

    订单服务注册进zookeeper

---
+++ consumerzk-order80/src/main/java/com/example/springcloud/config/ApplicationContextBean.java
@@ -0,0 +1,16 @@
+package com.example.springcloud.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class ApplicationContextBean {
+
+    @Bean
+    @LoadBalanced   //负载均衡，server.port
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
+}
