a5b22b65a3baaa43a5d879c449f03b65a576412b
HuangYuJue <11392488+huang-yujue@user.noreply.gitee.com>
2022-10-16 22:52:01 +0800

    Ribbon的负载均衡和Rest调用

--- consumer-order80/src/main/java/com/example/springcloud/controller/OrderController.java
+++ consumer-order80/src/main/java/com/example/springcloud/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.example.springcloud.controller;
 import com.example.springcloud.entities.CommonResult;
 import com.example.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,7 @@ import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
 
+// Ribbon = 负载均衡 + RestTemplate
 @RestController
 @Slf4j
 public class OrderController {
@@ -30,10 +32,40 @@ public class OrderController {
         return restTemplate.postForObject(PAYMENT_URL+"/payment/create",payment,CommonResult.class);
     }
 
+    @GetMapping("/consumer/postForEntity/create")
+    public CommonResult<Payment> create2(Payment payment){
+        ResponseEntity<CommonResult> entity = restTemplate.postForEntity(PAYMENT_URL + "/payment/create", payment, CommonResult.class);
+        //2xx即200是成功
+        if (entity.getStatusCode().is2xxSuccessful()){
+            return entity.getBody();
+        }else {
+            return new CommonResult<>(444,"操作失败");
+        }
+    }
+
     //因为是客户端，所以一般发的都是get请求
+    //返回对象为响应体中数据转化成的对象，基本上可以理解为Json：restTemplate.getForObject()
     @GetMapping("/consumer/payment/get/{id}")
     public CommonResult<Payment> getPaymentById(@PathVariable("id") Long id){
         //调用restTemplate中的get请求
         return restTemplate.getForObject(PAYMENT_URL+"/payment/get/"+id,CommonResult.class);
     }
+
+    //返回对象为ResponseEntity对象，包含了响应中的一些重要信息，比如响应头、响应状态码、响应体等：restTemplate.getForEntity()
+    @GetMapping(value = "/consumer/payment/getForEntity/{id}")
+    public CommonResult<Payment> getPayment(@PathVariable("id") Long id){
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+        //2xx即200是成功
+        if (entity.getStatusCode().is2xxSuccessful()){
+            //如果想要知道详细信息可以通过log方式
+            log.info("状态码："+entity.getStatusCode());
+            //返回请求体
+            return entity.getBody();
+        }else {
+            return new CommonResult<>(444,"操作失败");
+        }
+    }
+    /*
+    IRule：根据特定算法中从服务列表中选取一个要访问的服务
+     */
 }
