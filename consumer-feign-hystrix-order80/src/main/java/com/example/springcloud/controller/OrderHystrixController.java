a1fb8bf94f8637be4fc649ab4a0e6eabdf77b199
HuangYuJue <11392488+huang-yujue@user.noreply.gitee.com>
2022-10-23 18:21:54 +0800

    hystrix全局服务降级fallback

--- consumer-feign-hystrix-order80/src/main/java/com/example/springcloud/controller/OrderHystrixController.java
+++ consumer-feign-hystrix-order80/src/main/java/com/example/springcloud/controller/OrderHystrixController.java
@@ -1,6 +1,7 @@
 package com.example.springcloud.controller;
 
 import com.example.springcloud.service.PaymentHystrixService;
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.slf4j.Slf4j;
@@ -12,6 +13,8 @@ import javax.annotation.Resource;
 
 @RestController
 @Slf4j
+// 定义全局fallback方法(全局的兜底方法)
+@DefaultProperties(defaultFallback = "payment_Global_FallbackMethod")
 public class OrderHystrixController {
     @Resource
     private PaymentHystrixService paymentHystrixService;
@@ -33,9 +36,10 @@ public class OrderHystrixController {
             @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "1500")}
         表示timeoutInMilliseconds线程的超时时间是1500(1.5秒钟)，超过1.5秒钟则服务降级，调用备选兜底方法。
      */
-    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler",commandProperties = {
-            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "1500")
-    })
+//    @HystrixCommand(fallbackMethod = "paymentInfo_TimeOutHandler",commandProperties = {
+//            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "1500")
+//    })
+    @HystrixCommand //不指明fallback方法(兜底方法)，则用全局fallback方法(全局兜底方法)
     @GetMapping("/consumer/payment/hystrix/timeout/{id}")
     public String paymentInfo_TimeOut(@PathVariable("id") Integer id){
         String result = paymentHystrixService.paymentInfo_TimeOut(id);
@@ -43,5 +47,10 @@ public class OrderHystrixController {
     }
     public String paymentInfo_TimeOutHandler(Integer id){
         return "我是消费者80，对方支付系统繁忙请在10秒钟后再试或者自己运行出错检查自己，~^~";
+    }
+
+    // 下面是全局fallback方法(全局的兜底方法)
+    public String payment_Global_FallbackMethod(){
+        return "Global异常处理信息，请稍后再试 *_*";
     }
 }
