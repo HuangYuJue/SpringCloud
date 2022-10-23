bc2916a50da12c08d58845f500cfba8d6655382a
HuangYuJue <11392488+huang-yujue@user.noreply.gitee.com>
2022-10-23 13:06:30 +0800

    hystrix支付微服务构建

--- provider-hystrix-payment8001/src/main/java/com/example/springcloud/service/impl/PaymentServiceImpl.java
+++ provider-hystrix-payment8001/src/main/java/com/example/springcloud/service/impl/PaymentServiceImpl.java
@@ -10,6 +10,7 @@ public class PaymentServiceImpl implements PaymentService {
     //正常访问，一定OK
     public String paymentInfo_OK(Integer id){
         return "线程池： "+Thread.currentThread().getName()+" paymentInfo_OK,id:  "+id+"\t"+"ovo";
+        //线程池： http-nio-8001-exec-8 paymentInfo_OK,id: 6 ovo
     }
 
     //模拟睡眠3秒钟之后访问
@@ -22,6 +23,7 @@ public class PaymentServiceImpl implements PaymentService {
             e.printStackTrace();
         }
         return "线程池： "+Thread.currentThread().getName()+" paymentInfo_TimeOut,id:  "+id+"\t"+"ovo"+"  耗时(秒): "+timeNumber;
+        //线程池： http-nio-8001-exec-4 paymentInfo_TimeOut,id: 6 ovo 耗时(秒): 3
     }
 
 }
