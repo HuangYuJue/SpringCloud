63c31f637495d0986ab954303ab5ab203beb21ed
HuangYuJue <11392488+huang-yujue@user.noreply.gitee.com>
2022-10-23 13:31:30 +0800

    hystrix消费者构建

---
+++ consumer-feign-hystrix-order80/src/main/java/com/example/springcloud/controller/OrderHystrixController.java
@@ -0,0 +1,30 @@
+package com.example.springcloud.controller;
+
+import com.example.springcloud.service.PaymentHystrixService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class OrderHystrixController {
+    @Resource
+    private PaymentHystrixService paymentHystrixService;
+
+    //正常访问，一定OK
+    @GetMapping("/consumer/payment/hystrix/ok/{id}")
+    public String paymentInfo_OK(@PathVariable("id") Integer id){
+        String result = paymentHystrixService.paymentInfo_OK(id);
+        return result;
+    }
+
+    //模拟睡眠3秒钟之后访问
+    @GetMapping("/consumer/payment/hystrix/timeout/{id}")
+    public String paymentInfo_TimeOut(@PathVariable("id") Integer id){
+        String result = paymentHystrixService.paymentInfo_TimeOut(id);
+        return result;
+    }
+}
