bb54bb031b7506709288dbd300f78096dee99e83
HuangYuJue <11392488+huang-yujue@user.noreply.gitee.com>
2022-10-23 18:57:55 +0800

    hystrix全局服务降级DefaultProperties

--- consumer-feign-hystrix-order80/src/main/java/com/example/springcloud/controller/OrderHystrixController.java
+++ consumer-feign-hystrix-order80/src/main/java/com/example/springcloud/controller/OrderHystrixController.java
@@ -1,5 +1,4 @@
 package com.example.springcloud.controller;
-
 import com.example.springcloud.service.PaymentHystrixService;
 import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
@@ -8,7 +7,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
 import javax.annotation.Resource;
 
 @RestController
