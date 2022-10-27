8a30637bdc915926d077d9c61b213e1eac4b7b51
HuangYuJue <11392488+huang-yujue@user.noreply.gitee.com>
2022-10-27 23:12:11 +0800

    Config动态刷新之手动版

--- config-client3355/src/main/java/com/example/springcloud/ConfigClientMain3355.java
+++ config-client3355/src/main/java/com/example/springcloud/ConfigClientMain3355.java
@@ -2,12 +2,18 @@ package com.example.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * 输入：http://localhost:3355/configInfo返回GitHub里仓库中config-dev.yml里的config.info，
  * master branch,springcloud-config/config-dev.yml version=1
  */
+/*每次修改GitHub中文件的内容后在cmd中执行：
+curl -X POST "http://localhost:3355/actuator/refresh"，
+即可通过地址刷新得到修改后的内容。
+ */
+@RefreshScope   //实例热加载，实现刷新功能
 @SpringBootApplication
 @EnableEurekaClient
 public class ConfigClientMain3355 {
