29c9ff6a1a532f0537c75c163710cc008435edb0
HuangYuJue <11392488+huang-yujue@user.noreply.gitee.com>
2022-10-29 20:23:57 +0800

    Stream之消息重复消费

---
+++ stream-rabbitmq-consumer8803/src/main/java/com/example/springcloud/StreamMQMain8803.java
@@ -0,0 +1,31 @@
+package com.example.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 找到rabbitmq-server.bat双击，在地址栏输入http://localhost:15672即可访问rabbitmq。
+ * 消息驱动：整合rabbitmq以及Kafka中间体，类似于jdbc
+ * stream-rabbitmq-provider8801为消息的发送模块；
+ * stream-rabbitmq-consumer8802以及stream-rabbitmq-consumer8803为消息的接收模块。
+ */
+/*
+点击rabbitmq中Exchanges下的studyExchange，再点击Bindings里可以查看到绑定到了目标；
+如果消费者8802、8803都运行，则默认在Exchanges中的Bindings里会有两个绑定分组(还可在rabbitmq下的Queues下看到有几个绑定分组)，
+如果8801发送消息，8802和8803都会接收到相同的消息，则发送的消息被两个消费者接收到(重复消费)；
+eg:8801：******serial: f2fd4026-fa23-4fe5-8cf4-5bdc4da5dd5e
+8802：消费者1号，------>接收到的消息：f2fd4026-fa23-4fe5-8cf4-5bdc4da5dd5e	  port: 8802
+8803：消费者2号，------>接收到的消息：f2fd4026-fa23-4fe5-8cf4-5bdc4da5dd5e	  port: 8803
+重复消费问题：
+springcloud-stream目前案例是使用rabbitmq作为支持，现在默认的destination是topic形式，
+不同的微服务实例会共享到同一个topic，这个也是因为不同的微服务实例默认是不同组的，则在Exchanges中有几个组，
+不同组的微服务实例是全面消费(重复消费)；同组的微服务实例是竞争关系，能够保证消息只会被同一组的其中一个应用消费一次；
+如果要避免某些服务实例重复消费（这里的重复消费是：一个订单同时被多个服务获取到），就要进行分组。
+导致原因：默认分组group是不同的，组流水号不一样，被认为是不同组，表示可以消费。
+ */
+@SpringBootApplication
+public class StreamMQMain8803 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamMQMain8803.class,args);
+    }
+}
---
+++ stream-rabbitmq-consumer8803/src/main/java/com/example/springcloud/controller/ReceiveMessageListenerController.java
@@ -0,0 +1,25 @@
+package com.example.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableBinding(Sink.class)  //定义消息生产者的发送管道/消费者的接收管道，消息生产者里是Source，消息接收者里是Sink，指信道channel和exchange绑定在一起。
+public class ReceiveMessageListenerController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @StreamListener(Sink.INPUT) //监听队列，用于消费者的队列的消费接收，输入源
+    public void input(Message<String> message){
+        System.out.println("消费者2号，------>接收到的消息："+message.getPayload()+"\t  port: "+serverPort);
+        /*注意：
+        ①send()方法发的是String类型的信息，所以接收消息也是String，即Message<String> message；
+        ②信息中发的时候用的是withPayload()方法，所以收的时候是getPayload()方法；
+         */
+    }
+}
